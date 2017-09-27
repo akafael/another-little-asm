28cddde7da6f8a69230ebafc908ee1a1711448f8
Rafael <raffaellimma@gmail.com>
2017-09-27 10:36:28 -0300

    Primeira versão verificação de Macro

--- src/macroeval.cpp
+++ src/macroeval.cpp
@@ -8,6 +8,7 @@
 #include <string>
 #include <algorithm>
 
+#include "lexer.h"
 #include "msgs_pt.h"
 #include "macroeval.h"
 
@@ -31,17 +32,84 @@ int macroeval(int argc, char ** argv)
     }
 
     string line;
+    bool errorDetected = false;
+    vector<labelMacro> macroDefinitionTable;
+    labelMacro tmpMacroDef;
+
+    int currentStateMacroEval = MACROEV_READING_LINE;
 
     // Percorre todas as linhas do arquivo
     for(int lineCount=0;getline(ArquivoASM,line);lineCount++)
     {
        /// @todo Fazer validação das macros, IF e EQV
-       // Escreve linha no arquivo de saída
-       ArquivoPRE << line << '\n';
+
+       vector<token> vtoks = tokenizer(line);
+
+       // Verifica Sintaxe MACRO
+       if((vtoks.size()==3)&&(vtoks[0].type==WORD)&&(vtoks[1].type==COLON)\
+          &&(vtoks[2].type==WORD)&&(vtoks[2].string.compare("MACRO")==0))
+       {
+           int macroPos = findMacroDefinition(macroDefinitionTable,vtoks[0].string);
+           if(macroPos==MACRO_NOT_FOUND)
+           {
+               tmpMacroDef.text = vtoks[0].string;
+               tmpMacroDef.content = ""; // Esvazia conteúdo
+
+               currentStateMacroEval = MACROEV_WRITING_MACRO;
+           }
+           else
+           {
+               PRINT_ERR_LABEL_DUPLICATED(lineCount,line);
+           }
+       }// Verifica Sintaxe END:
+       else if((vtoks.size()==1)&&(vtoks[0].type==WORD)\
+           &&(vtoks[0].string.compare("END")==0))
+       {
+           currentStateMacroEval = MACROEV_READING_LINE;
+           macroDefinitionTable.push_back(tmpMacroDef);
+       }
+       else if((vtoks.size()!=0))
+       {
+           /// @todo Verificar uso das MACROS
+
+          for(vector<token>::iterator it = vtoks.begin(); it != vtoks.end();++it)
+          {
+              int macroPos = findMacroDefinition(macroDefinitionTable,(*it).string);
+              if(macroPos!=MACRO_NOT_FOUND)
+              {
+                  cout << MSG_ERR << macroPos;
+                  /// @todo Verificar necessidade de quebra de linha
+                   ArquivoPRE << macroDefinitionTable[macroPos].content;
+              }
+          }
+
+       }
+
+       if(currentStateMacroEval==MACROEV_WRITING_MACRO)
+       {
+           tmpMacroDef.content += line + '\n';
+       }
+       else
+       {
+           // Escreve linha no arquivo de saída
+           ArquivoPRE << line << '\n';
+       }
     }
 
     ArquivoPRE.close();
     ArquivoASM.close();
 
     return 0;
+}
+
+int findMacroDefinition(vector<labelMacro> macroDefinitionTable,string macroText)
+{
+    for (int i = 0; i <macroDefinitionTable.size(); i++)
+    {
+        if(macroText.compare(macroDefinitionTable[i].text)==0)
+        {
+            return i;
+        }
+    }
+    return MACRO_NOT_FOUND;
 }
--- src/macroeval.h
+++ src/macroeval.h
@@ -1,8 +1,25 @@
 #ifndef MACROEVAL_H_
 #define MACROEVAL_H_
 
+#include <string>
+#include <vector>
+
 using namespace std;
 
+typedef struct{
+    string text;
+    string content;
+} labelMacro;
+
+#define MACRO_NOT_FOUND -1
+
+typedef enum{
+    MACROEV_READING_LINE = 1,
+    MACROEV_WRITING_MACRO = 2,
+} StateMacroEvaluation;
+
 int macroeval(int argc, char ** argv);
+
+int findMacroDefinition(vector<labelMacro> macroDefinitionTable,string macroText);
 
 #endif
