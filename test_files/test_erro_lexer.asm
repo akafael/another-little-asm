6c7effcdba09237e00bd470edd9b54cd6d284129
Rafael <raffaellimma@gmail.com>
2017-10-03 10:56:55 -0300

    Arquivo de teste modificado

--- test_files/test_erro_lexer.asm
+++ test_files/test_erro_lexer.asm
@@ -1,5 +1,8 @@
 # ; Caracter inválido
-0x3A3C ; Número Hexadecimal
+0x3AXC ; Número Hexadecimal inválido
 0323B ; Número inválido
+-12323 ; Número Válido Negativo
+43589 ; Número Válido
+-0x1A3 ; Número Hexadecimal Válido
 nome ; Palavra Válida
 1begin ; Palavra inválida
