84d2b115c4653a5bb61230fc7ed2ca2c5a929edc
Rafael <raffaellimma@gmail.com>
2017-11-17 11:55:48 -0200

    Adicionado arquivo de testes para o simulador

---
+++ src/test_simulator.cpp
@@ -0,0 +1,12 @@
+#include "simulator.h"
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <algorithm>
+#include <vector>
+
+int main()
+{
+    simulator("../test_files/moodle_fat.obj");
+}
